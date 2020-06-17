cdb40b5dab72ef753567e820eb6d1ccb458a5a11
Mukul Singhal <46862980+Mukul-Singhal@users.noreply.github.com>
2020-06-17 12:23:03 +0530

    styling HomePage

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Directory from "../../components/directory/directory";
+import "./homepage.scss";
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+export default Homepage;
