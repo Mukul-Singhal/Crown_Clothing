2fa9169e317bb3793b67f2c1808b737c56e5a0ff
Mukul Singhal <46862980+Mukul-Singhal@users.noreply.github.com>
2020-06-17 14:13:29 +0530

    Routing in menu-items using HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Homepage from "./pages/homepage/homepage";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
+const Hatspage = () => <div>hello</div>;
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route path="/shop/hats" component={Hatspage}></Route>
+        <Route path="/" component={Homepage}></Route>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -41,8 +41,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.section.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.section.map(({ id, ...othersections }) => (
+          <MenuItem key={id} {...othersections} />
         ))}
       </div>
     );
